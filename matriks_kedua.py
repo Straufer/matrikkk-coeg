711451623ee07b0824594d2dd576339405220bdc
straufer <paungcuy15@gmail.com>
2023-08-20 17:39:09 +0700

    update final

--- matriks_kedua.py
+++ matriks_kedua.py
@@ -11,10 +11,31 @@ r = np.array([[4, 3], [-2, 2]])
 # 1. pq
 # 2. pq^t
 # 3. p(r+q)
-# 4. (p+q)(p-q])
+# 4. (p+q)(p-q)
+ 
+# tebakan
+# 1. -6, 4, 3, -24
+# 2. -2, -4, 23, -20
+# 3. 0, 7, -11, -17
+# 4. -10, 48, -6, 12
 
 # 1
 soal_1 = np.dot(p, q)
-print(soal_1)
+print(f"soal 1: \n{soal_1}")
+
+# 2
+soal_2 = np.dot(p, np.transpose(q))
+print(f"soal 2: \n{soal_2}")
+
+# 3
+sum_rq = np.add(r, q)
+soal_3 = np.dot(p, sum_rq)
+print(f"soal 3: \n{soal_3}")
+
+# 4 
+sum_plus = np.add(p, q)
+sum_mines = np.subtract(p, q)
+soal_4 = np.dot(sum_plus, sum_mines)
+print(f"soal 4: \n{soal_4}")
 
 
